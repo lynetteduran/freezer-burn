77741db3d13e097e0129caad8e09ca7cb581af8f
Lynette Duran <lyndduran@gmail.com>
2018-05-04 09:36:06 -0700

    fixes naming issues in ./cllent/src/ files

---
+++ client/src/FreezerBox.js
@@ -0,0 +1,30 @@
+<<<<<<< Updated upstream
+=======
+import React, { Component } from 'react';
+import FreezerList from './FreezerList';
+import FreezerForm from './FreezerForm';
+import DATA from './data';
+import './FreezerBox.css';
+
+class FreezerBox extends Component {
+  constructor() {
+    super();
+    this.state = { data: [] };
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="freezers">
+          <h2>Freezers:</h2>
+          <FreezerList data={DATA} />
+        </div>
+        <div className="form">
+          <FreezerForm />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FreezerBox;
+>>>>>>> Stashed changes
