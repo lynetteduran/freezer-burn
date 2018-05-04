e5c9fbbd2d200af8bbaef4bdffd4ff994214586c
Lynette Duran <lyndduran@gmail.com>
2018-05-04 07:54:02 -0700

    creates Freezer component

---
+++ client/src/Freezer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactMarkdown from 'react-markdown';
+
+const Freezer = props => (
+  <div className= "singleFreezer">
+    <div className= "textContent">
+      <div className= "singleFreezerContent">
+        <h3>freezer</h3>
+        <h3>{props.freezerNum}</h3>
+        <ReactMarkdown source= {props.children} />
+      </div>
+      <div className= "singleFreezerButtons">
+      </div>
+    </div>
+  </div>
+);
+
+Freezer.propTypes = {
+  freezerNum: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
+  timestamp: PropTypes.string.isRequired,
+}
+
+export default Freezer;
