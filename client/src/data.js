0063b7b52c6e1c8d6ab3e678689e51542e9d55f4
Lynette Duran <lyndduran@gmail.com>
2018-05-04 08:36:24 -0700

    creates seedfile data.js

---
+++ client/src/data.js
@@ -0,0 +1,5 @@
+const data = [
+  {_id: 1, freezerNum: '1', freezerLoc: 'Main Room', updatedAt: new Date(), createdAt: new Date()},
+
+  {_id: 2,freezerNum: '2', freezerLoc: 'Back Room', updatedAt: new Date(), createdAt: new Date()} }
+]
