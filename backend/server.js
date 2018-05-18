7c34711815f5ef6de3e23cf501474c6c0d637bfb
Lynette Duran <lyndduran@gmail.com>
2018-05-18 03:18:14 -0700

    package.json update

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,24 @@
 /*IMPORT DEPENDENCIES*/
 import express from 'express';
-import bodyParse from 'body-parser';
+import bodyParser from 'body-parser';
 import logger from 'morgan';
 import mongoose from 'mongoose';
 
-/*APP, ROUTER AND API_PORT CONSTRUCTORS*/
+/*APP, ROUTER AND API_PORT CONSTRUCTORS FOR INSTANCES*/
 const app = express();
 const router = express.Router();
 const API_PORT = process.env.API_PORT || 3000;
 
 /*SETS DEPENDENCIES FOR APP INSTANCES*/
-app.use(bodyParser.urlencoded({ extended: false} ));
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
-app.(logger('dev'));
+app.use(logger('dev'));
 
 /*SETS HOMEPAGE AND API*/
-router.get('/', (req,res) => {
-  res.json({ message: 'Hello, world!'})
+router.get('/', (req, res) => {
+  res.json({ message: 'Hello, world!'});
 });
-app.use('/api', router);
-
-/*SETS PORT LISTENER*/
-app.listen(API_PORT, () => console.log('I can hear you breath...on port ${API_PORT}'));
+  // app.use('/api', router);
+  //
+  // /*SETS PORT LISTENER*/
+  // app.listen(API_PORT, () => console.log('I can hear you breath...on port' + ' ' + API_PORT));
