cbe81d8d551fbbe082af77910792f9d3a9f90dc3
Lynette Duran <lyndduran@gmail.com>
2018-12-09 20:09:50 -0800

    creates freezer schema

---
+++ backend/models/freezer.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const FreezersSchema = new Schema({
+  freezerNum: Number,
+  freezerLoc: String,
+}, { timestamps: true});
+
+export default mongoose.model('Freezer', FreezerSchema);
