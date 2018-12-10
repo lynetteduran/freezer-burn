a7d7a9b6aaf8cccb223fb25dbff03cbccd9b88e4
Lynette Duran <lyndduran@gmail.com>
2018-12-09 20:11:00 -0800

    integrates mLab remote db

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import mongoose from 'mongoose';
+import { getSecret } from './secrets';
+import Freezer from './models/freezer';
 
 /*APP, ROUTER AND API_PORT CONSTRUCTORS*/
 const app = express();
 const router = express.Router();
 const API_PORT = process.env.API_PORT || 3000;
+
+/* DB CONFIG - SETS URI FROM MLAB IN secrets.js*/
+mongoose.connect(getSecret('dbUri'));
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 /*SETS DEPENDENCIES FOR APP INSTANCES*/
 app.use(bodyParser.urlencoded({ extended: false} ));
