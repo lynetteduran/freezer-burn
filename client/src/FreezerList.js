0c9dfe1128327086a6b89ae974bf05791821020e
Lynette Duran <lyndduran@gmail.com>
2019-03-19 05:49:45 -0700

    passes put and delete methods into FreezerList.js

--- client/src/FreezerList.js
+++ client/src/FreezerList.js
@@ -4,20 +4,21 @@ import Freezer from './Freezer';
 
 const FreezerList = (props) => {
   const freezerNodes = props.data.map(freezer => (
-
     <Freezer
       freezerNum = {freezer.freezerNum}
       freezerLoc = {freezer.freezerLoc}
-      key= {freezer._id}
-      id= {freezer._id}>
-      timestamp={freezer.updatedAt}
-
+      key = {freezer._id}
+      id = {freezer._id}>
+      timestamp = {freezer.updatedAt}
+      handleUpdateFreezer = {props.handleUpdateFreezer}
+      handleDeleteFreezer = {props.handleDeleteFreezer}>
+      {freezer.text}
     </Freezer>
   ));
 
   return (
     <div>
-      {freezerNodes}
+      { freezerNodes }
     </div>
   );
 }
@@ -30,7 +31,10 @@ FreezerList.propTypes = {
     freezerLoc: PropTypes.string,
     id: PropTypes.string,
     text: PropTypes.string,
+    updatedAt: PropTypes.string,
   })),
+  handleDeleteFreezer: PropTypes.func.isRequired,
+  handleUpdateFreezer: PropTypes.func.isRequired,
 }
 
 FreezerList.defaultProps = {
